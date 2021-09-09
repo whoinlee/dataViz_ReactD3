80fd5825da74b8bdae31ba52a7cb43f0f9548d8f
Whoin Lee <whoinlee@gmail.com>
2021-09-09 13:21:17 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+function App() {
+  return (
+    <div className="App">
+      <svg width="960" height="500">
+        <circle r="200" 
+                cx="480"
+                cy="245"
+                fill="yellow" stroke="black" strokeWidth="10">
+        </circle>
+      </svg>
+    </div>
+  );
+}
+
+export default App;
