eafa2e5bbc842bc0639fbb79131953cd12a6d697
Whoin Lee <whoinlee@gmail.com>
2021-09-09 13:41:40 -0400

    d3 added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,38 @@
+import {arc} from 'd3';
 
-function App() {
+
+
+const App = () => {
+  const width = 960;
+  const height = 500;
+  const strokeW = 10;
+  const centerX = width/2;
+  const centerY = (height-strokeW)/2;
+  const offsetX = 85;
+  const offsetY = 55;
+
+  console.log(arc);
   return (
     <div className="App">
-      <svg width="960" height="500">
+      <svg width={width} height={height}>
         <circle r="200" 
-                cx="480"
-                cy="245"
-                fill="yellow" stroke="black" strokeWidth="10">
+                cx={centerX}
+                cy={centerY}
+                fill="yellow" stroke="black" strokeWidth={strokeW}>
+        </circle>
+        <circle r="40" 
+                    cx={centerX-offsetX}
+                    cy={centerY-offsetY}
+                    fill="black">
+        </circle>
+        <circle r="40" 
+                cx={centerX+offsetX}
+                cy={centerY-offsetY}
+                fill="black" >
         </circle>
       </svg>
     </div>
   );
-}
+};
 
 export default App;
