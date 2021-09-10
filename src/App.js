5d1c1cffb749b4ef1df7183d56d3af2531d89f13
Whoin Lee <whoinlee@gmail.com>
2021-09-10 08:40:53 -0400

    data prep & fetch data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import {arc} from 'd3';
 
 
-
 const App = () => {
   const width = 960;
   const height = 500;
@@ -10,28 +9,25 @@ const App = () => {
   const centerY = (height-strokeW)/2;
   const offsetX = 85;
   const offsetY = 55;
+  const mouthArc = arc()
+    .innerRadius(140)
+    .outerRadius(150)
+    .startAngle(Math.PI/2 + Math.PI/6)
+    .endAngle(Math.PI + Math.PI/2 - Math.PI/6);
 
-  console.log(arc);
   return (
-    <div className="App">
-      <svg width={width} height={height}>
-        <circle r="200" 
-                cx={centerX}
-                cy={centerY}
-                fill="yellow" stroke="black" strokeWidth={strokeW}>
-        </circle>
-        <circle r="40" 
-                    cx={centerX-offsetX}
-                    cy={centerY-offsetY}
-                    fill="black">
-        </circle>
-        <circle r="40" 
-                cx={centerX+offsetX}
-                cy={centerY-offsetY}
-                fill="black" >
-        </circle>
-      </svg>
-    </div>
+        <svg width={width} height={height}>
+          <g transform={`translate(${centerX}, ${centerY})`}>
+            <circle r="200" fill="yellow" stroke="black" strokeWidth={strokeW} />
+            <circle r="40" fill="black"
+                    cx={-offsetX}
+                    cy={-offsetY} />
+            <circle r="40" fill="black"
+                    cx={offsetX}
+                    cy={-offsetY} />
+            <path d={mouthArc()} />
+          </g>
+        </svg>
   );
 };
 
