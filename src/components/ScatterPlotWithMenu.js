8e736a87b487ead8fecddcc0546e8f89a42397ca
Whoin Lee <whoinlee@gmail.com>
2021-09-23 15:49:06 -0400

    dropdown menu added for scatter plot

--- src/components/ScatterPlotWithMenu.js
+++ src/components/ScatterPlotWithMenu.js
@@ -4,13 +4,12 @@ import { scaleLinear, extent, format } from 'd3';
 import { useIrisData } from '../utils/useIrisData';
 import ReactDropdown from 'react-dropdown';
 //-- Components
-import AxisBottom from './AxisBottom';
-import AxisLeftScatter from './AxisLeftScatter';
-import ScatterMarks from './ScatterMarks';
-// import Dropdown from './Dropdown';
+import AxisBottom from './axis/AxisBottom';
+import AxisLeftS from './axis/AxisLeftS';
+import ScatterMarks from './marks/ScatterMarks';
 //-- Styles
-import "../styles/ScatterPlotWithMenu.css"; 
 import "react-dropdown/style.css";
+import "../styles/ScatterPlotWithMenu.css"; 
 
 
 const width=980;
@@ -54,9 +53,8 @@ const ScatterPlotWithMenu = () => {
   if (!data) {
     return <pre>Loading ... </pre>
   } 
-
   console.log("data\n", data);
-  console.log("data.columns\n", data.columns);
+  // console.log("data.columns\n", data.columns);
 
   const innerHeight = height - margin.bottom - margin.top;
   const innerWidth = width - margin.right - margin.left;
@@ -66,12 +64,11 @@ const ScatterPlotWithMenu = () => {
     .domain(extent(data, xValue))
     .range([0, innerWidth])
     .nice();  //*****/
-
   const yScale = scaleLinear()
     .domain(extent(data, yValue))
     .range([0, innerHeight])
     .nice();
-  
+
   const xAxisTickFormat = tickValue => format(".2s")(tickValue).replace("G", "B");
   const yAxisLabelOffset = 45;
 
@@ -79,27 +76,27 @@ const ScatterPlotWithMenu = () => {
   <div>
     <div className="menus-container">
       <span className="dropdown-label">X</span>
-      <ReactDropdown options={attributes}
-                  value={xAttribute}
-                  onChange={({value}) => setXAttribute(value)} />
+      <ReactDropdown  options={attributes}
+                      value={xAttribute}
+                      onChange={({value}) => setXAttribute(value)} />
       <span className="dropdown-label">Y</span>
-      <ReactDropdown options={attributes}
-                  value={yAttribute}
-                  onChange={({value}) => setYAttribute(value)} />
+      <ReactDropdown  options={attributes}
+                      value={yAttribute}
+                      onChange={({value}) => setYAttribute(value)} />
     </div>
     <svg width={width} height={height}>
       <g transform={`translate(${margin.left}, ${margin.top})`}>
-          
           <AxisBottom xScale={xScale} innerHeight={innerHeight} 
-                      tickFormat={xAxisTickFormat} />
-          <text transform={`translate(${-yAxisLabelOffset}, ${innerHeight/2}) rotate(-90)`}
-                textAnchor="middle">{yAxisLabel}</text>
-          <AxisLeftScatter yScale={yScale} innerWidth={innerWidth}/>
+                      tickFormat={xAxisTickFormat} tickOffset={5}/>
           <text x={innerWidth/2} y={innerHeight+45} 
                 textAnchor="middle">{xAxisLabel}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</text>
-          <ScatterMarks data={data}  xScale={xScale} yScale={yScale} 
-                              xValue={xValue} yValue={yValue}
-                              tooltipFormat={xAxisTickFormat} cRadius={8}/>
+          <AxisLeftS yScale={yScale} innerWidth={innerWidth}/>
+          <text transform={`translate(${-yAxisLabelOffset}, ${innerHeight/2}) rotate(-90)`}
+                textAnchor="middle">{yAxisLabel}</text>
+          <ScatterMarks data={data}  
+                        xScale={xScale} yScale={yScale} 
+                        xValue={xValue} yValue={yValue}
+                        tooltipFormat={xAxisTickFormat} cRadius={8}/>
       </g>
     </svg>
   </div>)
--- src/index.js
+++ src/index.js
@@ -4,15 +4,16 @@ import ReactDOM from 'react-dom';
 // import DonutChart from './components/DonutChart';
 // import BarChart from './components/BarChart';
 // import ScatterPlot from './components/ScatterPlot';
+import ScatterPlotWithMenu from './components/ScatterPlotWithMenu';
 // import LineChart from './components/LineChart';
-import WorldMap from './components/WorldMap';
+// import WorldMap from './components/WorldMap';
 //-- Styles
 import "./style.css";
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <WorldMap />
+    <ScatterPlotWithMenu />
   </React.StrictMode>,
   document.getElementById('root')
 );
